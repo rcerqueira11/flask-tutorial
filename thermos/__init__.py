a1ca8dd5733186fe590b6b5318d700a0b272ca53
Ricardo Cerqueira <raxs64@gmail.com>
2017-08-21 14:12:11 -0400

    thermos updated

---
+++ thermos/__init__.py
@@ -0,0 +1,26 @@
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+basedir= os.path.abspath(os.path.dirname(__file__))
+
+#configure database
+app= Flask(__name__)
+app.config['SECRET_KEY'] = '\x05\xa5\xa6\xb8\xe8\x06\x0f\xa9\x98\xf4\xf8\xe5H\x92j9\xab\x16\xe5\xe0\xa8\x9e\xb9\x92'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'thermos.db')
+app.config['DEBUG']= True
+db = SQLAlchemy(app)
+
+
+#configure authentication
+login_manager = LoginManager()
+login_manager.session_protection = "strong"
+#to redirect when no logged in
+login_manager.login_view = "login"
+login_manager.init_app(app)
+
+
+import models
+import views
---
+++ thermos/views.py
@@ -0,0 +1,61 @@
+from flask import render_template, url_for, request, redirect, flash
+from flask_login import login_required, login_user, logout_user, current_user
+
+from thermos import app, db, login_manager
+from forms import BookmarkForm, LoginForm
+from models import User, Bookmark
+
+
+@login_manager.user_loader
+def load_user(userid):
+    return User.query.get(int(userid))
+
+@app.route('/') 
+@app.route('/index')
+def index():
+    return render_template('index.html', new_bookmarks=Bookmark.newest(5))
+
+# @app.route('/add')
+@app.route('/add', methods = ['GET','POST'])
+@login_required
+def add():
+    form = BookmarkForm()
+    if form.validate_on_submit():
+        url = form.url.data
+        description = form.description.data
+        bm = Bookmark(user= current_user, url=url, description=description)
+        db.session.add(bm)
+        db.session.commit()
+        flash("Sored bookmark '{}'".format(index))
+        return redirect(url_for('index'))
+    return render_template('add.html',form=form)
+
+
+@app.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html',user=user)
+
+# to use the 404 error page we use handler error
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+    
+# to use the 500 error page we use handler error
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+       #login validate the user..
+       user = User.query.filter_by(username=form.username.data).first()
+       if user is not None:
+           login_user(user, form.remember_me.data)
+           flash("Logged in successfully as {}.".format(user.username))
+           return redirect(request.args.get("next") or url_for('index'))
+        #    request.args.get("next"): salva la pagina donde queriamos acceder para que cuando nos logueemos nos redirecione a dicha pagina
+    flash("Incorrect username or password")
+    return render_template("login.html", form=form)
