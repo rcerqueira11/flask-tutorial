aefca981d2f71f2aba43275680cb48a35bee92f5
Ricardo Cerqueira <raxs64@gmail.com>
2017-08-22 17:13:32 -0400

    debug tool

--- thermos/__init__.py
+++ thermos/__init__.py
@@ -4,11 +4,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask.ext.moment import Moment
+from flask_debugtoolbar import DebugToolbarExtension
+
+app= Flask(__name__)
 
 basedir= os.path.abspath(os.path.dirname(__file__))
 
 #configure database
-app= Flask(__name__)
 app.config['SECRET_KEY'] = '\x05\xa5\xa6\xb8\xe8\x06\x0f\xa9\x98\xf4\xf8\xe5H\x92j9\xab\x16\xe5\xe0\xa8\x9e\xb9\x92'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'thermos.db')
 app.config['DEBUG']= True
@@ -22,8 +24,14 @@ login_manager.session_protection = "strong"
 login_manager.login_view = "login"
 login_manager.init_app(app)
 
+#enable debugtoolbar
+toolbar = DebugToolbarExtension(app)
+
+
 # for displaying timestamps
 moment = Moment(app)
+
+
 
 import models
 import views
--- thermos/models.py
+++ thermos/models.py
@@ -17,8 +17,9 @@ class Bookmark(db.Model):
     date = db.Column(db.DateTime, default = datetime.utcnow)
     description = db.Column(db.String(300))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    _tags = db.relationship('Tag',secondary=tags, backref = db.backref('bookmarks', lazy='dynamic'))
-    
+    _tags = db.relationship('Tag',secondary=tags, lazy='joined',
+                            backref = db.backref('bookmarks', lazy='dynamic'))
+
     @staticmethod
     def newest(num):
         return Bookmark.query.order_by(desc(Bookmark.date)).limit(num)
